065e39ef9873e9a99a6b81509359ba6781697fab
rocilq <roziolq@gmail.com>
2023-03-31 20:09:04 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private CharacterController controller;
+    private Vector3 direction;
+    public float forwardSpeed;
+
+    private int desiredLane = 1;
+    public float laneDistance = 4; // the distance between two lanes
+
+    public float jumpForce;
+
+    public float Gravity = - 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.z = forwardSpeed;
+
+        
+
+        if (controller.isGrounded)
+        {
+            direction.y = -1;
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            direction.y += Gravity * Time.deltaTime;
+        }
+
+        // Gather the inputs on which lane we should be
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredLane++;
+            if(desiredLane == 3)
+            {
+                desiredLane = 2;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLane--;
+            if (desiredLane == -1)
+            {
+                desiredLane = 0;
+            }
+        }
+
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        if(desiredLane == 0)
+        {
+            targetPosition += Vector3.left * laneDistance;
+        }else if(desiredLane == 2)
+        {
+            targetPosition += Vector3.right * laneDistance;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 80 * Time.fixedDeltaTime);
+
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(direction * Time.fixedDeltaTime);
+    }
+
+    private void Jump()
+    {
+        direction.y = jumpForce;
+    }
+}
