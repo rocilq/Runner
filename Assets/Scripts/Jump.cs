454ed39cdfb539580e018d535bb38d8b3aa21ea7
rocilq <roziolq@gmail.com>
2023-05-12 19:44:04 +0200

    doble salto

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-
     public float jumpForce = 5f;
     public Animator animator;
+    private bool hasJumped = false;
+    private bool isGrounded = false;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundDistance = 0.2f;
 
     void Start()
     {
@@ -16,13 +20,25 @@ public class Jump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
 
-        if (SwipeManager.swipeUp) { 
-           
+        if (!hasJumped && isGrounded && SwipeManager.swipeUp)
+        {
+            // Realizar el salto solo si no se ha saltado antes y está en el suelo
             GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             animator.SetTrigger("jumpTrigger");
-
+            hasJumped = true;
         }
+
+        ResetJump();
     }
 
+    public void ResetJump()
+    {
+        // Restablecer el estado del salto
+        if (isGrounded)
+        {
+            hasJumped = false;
+        }
+    }
 }
